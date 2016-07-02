4d4f0c73c9eed80c6cd2ffeedd0742884cd47c5f
imran khan <imran233113@gmail.com>
2016-07-02 22:19:39 +0530

    Initial Commit

---
+++ TableViewPopUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewPopUp
+//
+//  Created by Imran Khan on 02/07/16.
+//  Copyright © 2016 IK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewPopUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewPopUp
+//
+//  Created by Imran Khan on 02/07/16.
+//  Copyright © 2016 IK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
